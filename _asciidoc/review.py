3778e9bb33bd16c58232a5180fef5f818c68ef4c
Eric Berdahl <berdahl@serendipity.org>
2018-09-20 09:47:40 +0200

    Minor physical refactoring of data. Pulled variables specific to band review (e.g. vs winter show) into variables. Immediate value is to clarify configuration data. Longer term, these could be externalized.

--- _asciidoc/review.py
+++ _asciidoc/review.py
@@ -5,19 +5,20 @@ import os
 import pybars
 import yaml
 
-def renderPystache(viewdata):
-    import pystache
-    renderer = pystache.Renderer(file_encoding='utf-8', file_extension='adoc')
-    print renderer.render(u"{{> band-review}}", viewdata)
+eventDataPath = '../_data/bandreview.yml'
+schoolDataDir = '../_data/schools'
+partialsDir = '../_partials'
+rootPartial = 'band-review'
+outputAdocFilepath = 'test.adoc'
 
 def readUTF8(filename):
     with io.open(filename, mode='rt', encoding='utf-8') as f:
         return f.read()
 
 def collectViewdata():
-    data = yaml.load(readUTF8('../_data/bandreview.yml'))
+    data = yaml.load(readUTF8(eventDataPath))
     schools = { }
-    for root, dirs, files in os.walk('../_data/schools'):
+    for root, dirs, files in os.walk(schoolDataDir):
         for f in files:
             filename, extension = os.path.splitext(f)
             if (extension == '.yml'):
@@ -37,10 +38,11 @@ def collectPartials(basePath, compiler):
 def main():
     compiler = pybars.Compiler()
     viewdata = collectViewdata()
-    partials = collectPartials('../_partials', compiler)
+    partials = collectPartials(partialsDir, compiler)
     
-    template = compiler.compile(u"{{> band-review}}")
-    with io.open('test.adoc', mode='wt', encoding='utf-8') as f:
+    rootTemplate = '{{> ' + rootPartial + ' }}'
+    template = compiler.compile(unicode(rootTemplate))
+    with io.open(outputAdocFilepath, mode='wt', encoding='utf-8') as f:
         f.write(template(viewdata, partials=partials))
 
 main()
