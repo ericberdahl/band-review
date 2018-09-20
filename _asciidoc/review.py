3000e31f355c82785ba0ae9d80a7962f2905f518
Eric Berdahl <berdahl@serendipity.org>
2018-09-20 10:14:14 +0200

    Additional code massage in prep for turning the specific script into one that accepts arguments for flexibility.

--- _asciidoc/review.py
+++ _asciidoc/review.py
@@ -3,19 +3,15 @@
 import io
 import os
 import pybars
+import subprocess
+import sys
 import yaml
-
-eventDataPath = '../_data/bandreview.yml'
-schoolDataDir = '../_data/schools'
-partialsDir = '../_partials'
-rootPartial = 'band-review'
-outputAdocFilepath = 'test.adoc'
 
 def readUTF8(filename):
     with io.open(filename, mode='rt', encoding='utf-8') as f:
         return f.read()
 
-def collectViewdata():
+def collectViewdata(eventDataPath, schoolDataDir):
     data = yaml.load(readUTF8(eventDataPath))
     schools = { }
     for root, dirs, files in os.walk(schoolDataDir):
@@ -35,14 +31,40 @@ def collectPartials(basePath, compiler):
                 partials[filename] = compiler.compile(readUTF8(os.path.join(root, f)))
     return partials
 
-def main():
+def buildBook(eventDataPath, schoolDataDir, partialsDir, rootPartial):
+    rootTemplate = '{{> ' + rootPartial + ' }}'
+    adocFilepath = rootPartial + '-book.adoc'
+    pdfFilepath = rootPartial + '-book.pdf'
+
     compiler = pybars.Compiler()
-    viewdata = collectViewdata()
+
+    print '# Collecting data'
+    viewdata = collectViewdata(eventDataPath, schoolDataDir)
+
+    print '# Compiling partials'
     partials = collectPartials(partialsDir, compiler)
     
-    rootTemplate = '{{> ' + rootPartial + ' }}'
+    print '# Compiling asciidoc book {}'.format(adocFilepath)
     template = compiler.compile(unicode(rootTemplate))
-    with io.open(outputAdocFilepath, mode='wt', encoding='utf-8') as f:
+    with io.open(adocFilepath, mode='wt', encoding='utf-8') as f:
         f.write(template(viewdata, partials=partials))
+    
+    print '# Compiling pdf book {}'.format(pdfFilepath)
+    subprocess.call([
+        'asciidoctor-pdf',
+        '-a', 'pdf-stylesdir=.',
+        '-a', 'pdf-style=announcer',
+        adocFilepath,
+        '-o', pdfFilepath
+        ])
 
-main()
+def main(argv):
+    eventDataPath = '../_data/bandreview.yml'
+    schoolDataDir = '../_data/schools'
+    partialsDir = '../_partials'
+    rootPartial = 'band-review'
+
+    buildBook(eventDataPath, schoolDataDir, partialsDir, rootPartial)
+
+if __name__ == "__main__":
+	main(sys.argv)
